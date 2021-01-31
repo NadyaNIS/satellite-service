ee324f53daf43308004af25b569a8828164c95ba
NadyaNIS <nadyaimanthie@gmail.com>
2021-01-31 10:02:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { PORT = 8000 } = process.env;
 
 
 app.get('/', function (req, res) {
-  res.send('Satellite Service version 1.1')
+  res.send('Satellite Service version 1.2')
 })
 
 app.use(bodyParser.urlencoded({ extended: true }));
